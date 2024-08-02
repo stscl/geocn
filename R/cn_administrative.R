d890e5958070fabaf916ec1dbb8a3961877ae50b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-02 11:43:51 +0800

    update load_cn_* function

--- R/cn_administrative.R
+++ R/cn_administrative.R
@@ -1,4 +1,4 @@
-#' Loading Chinese Provincial Administrative Divisional Polygon Data
+#' Loading Chinese Province-Level Administrative Divisional Polygon Data
 #'
 #' @param simplify (optional) Whether to simplify loading sf objects (default TRUE)
 #' @param keep (optional) Proportion of points to retain (0-1; default 0.05)
@@ -10,15 +10,15 @@
 #'
 #' @examples
 #' library(sf)
-#' province = load_province_cn()
+#' province = load_cn_province()
 #' province
 #'
-load_province_cn = \(simplify = TRUE,
+load_cn_province = \(simplify = TRUE,
                      keep = 0.05,
                      keep_shape = TRUE,
                      ...){
   cn_province = system.file('extdata/china.gdb',package = 'geocn') |>
-    sf::read_sf(layer = '省')
+    sf::read_sf(layer = 'province')
 
   if (simplify){
     cn_province = cn_province |>
@@ -26,4 +26,64 @@ load_province_cn = \(simplify = TRUE,
       sf::st_cast('MULTIPOLYGON')
   }
   return(cn_province)
+}
+
+#' Loading Chinese City-Level Administrative Division Polygon Data
+#'
+#' @param simplify (optional) Whether to simplify loading sf objects (default TRUE)
+#' @param keep (optional) Proportion of points to retain (0-1; default 0.05)
+#' @param keep_shape (optional)	Prevent small polygon features from disappearing at high simplification (default TRUE)
+#' @param ... (optional) Other arguments passed to `rmapshaper::ms_simplify()`
+#'
+#' @return Whether to simplify loading sf objects
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' city = load_cn_city()
+#' city
+#'
+load_cn_city = \(simplify = TRUE,
+                 keep = 0.05,
+                 keep_shape = TRUE,
+                 ...){
+  cn_city = system.file('extdata/china.gdb',package = 'geocn') |>
+    sf::read_sf(layer = 'city')
+
+  if (simplify){
+    cn_city = cn_city |>
+      rmapshaper::ms_simplify(keep = keep,keep_shapes = keep_shape,...) |>
+      sf::st_cast('MULTIPOLYGON')
+  }
+  return(cn_city)
+}
+
+#' Loading Chinese County-Level Administrative Division Polygon Data
+#'
+#' @param simplify (optional) Whether to simplify loading sf objects (default TRUE)
+#' @param keep (optional) Proportion of points to retain (0-1; default 0.05)
+#' @param keep_shape (optional)	Prevent small polygon features from disappearing at high simplification (default TRUE)
+#' @param ... (optional) Other arguments passed to `rmapshaper::ms_simplify()`
+#'
+#' @return Whether to simplify loading sf objects
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' county = load_cn_county()
+#' county
+#'
+load_cn_county = \(simplify = TRUE,
+                   keep = 0.05,
+                   keep_shape = TRUE,
+                 ...){
+  cn_county = system.file('extdata/china.gdb',package = 'geocn') |>
+    sf::read_sf(layer = 'county')
+
+  if (simplify){
+    cn_county = cn_county |>
+      rmapshaper::ms_simplify(keep = keep,keep_shapes = keep_shape,...) |>
+      sf::st_cast('MULTIPOLYGON')
+  }
+  return(cn_county)
 }
