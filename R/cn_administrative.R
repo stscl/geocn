699f630bedc7e46a05b7ce4023ae883f33848a4f
SpatLyu <lyu.geosocial@gmail.com>
2024-08-02 11:12:56 +0800

    add document

--- R/cn_administrative.R
+++ R/cn_administrative.R
@@ -1,3 +1,18 @@
+#' Loading Chinese Provincial Administrative Divisional Polygon Data
+#'
+#' @param simplify (optional) Whether to simplify loading sf objects (default TRUE)
+#' @param keep (optional) Proportion of points to retain (0-1; default 0.05)
+#' @param keep_shape (optional)	Prevent small polygon features from disappearing at high simplification (default TRUE)
+#' @param ... (optional) Other arguments passed to `rmapshaper::ms_simplify()`
+#'
+#' @return Whether to simplify loading sf objects
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' province = load_province_cn()
+#' province
+#'
 load_province_cn = \(simplify = TRUE,
                      keep = 0.05,
                      keep_shape = TRUE,
