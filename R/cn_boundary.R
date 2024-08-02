ac407accd6b563b1ad21504f343cf059c3b3bd6b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-02 22:21:09 +0800

    add load_cn_landborder function

--- R/cn_boundary.R
+++ R/cn_boundary.R
@@ -12,3 +12,18 @@ load_cn_landcoast = \(){
     sf::read_sf(layer = 'landcoast')
   return(cn_landcoast)
 }
+
+#' Loading China's Land Border
+#'
+#' @return An sf object
+#' @export
+#'
+#' @examples
+#' cn_landborder = load_cn_landborder()
+#' cn_landborder
+#'
+load_cn_landborder = \(){
+  cn_landborder = system.file('extdata/china.gdb',package = 'geocn') |>
+    sf::read_sf(layer = 'landborder')
+  return(cn_landborder)
+}
