17993511bf32f352f10d3939217c53e466326004
SpatLyu <lyu.geosocial@gmail.com>
2024-08-04 19:18:53 +0800

    update load_weihe_basin output to polygon

--- R/cn_typical_region.R
+++ R/cn_typical_region.R
@@ -7,9 +7,13 @@
 #' load_weihe_basin()
 #'
 load_weihe_basin = \(){
-  cn_weihe_basin = system.file('extdata/weihe.gdb',package = 'geocn') |>
-    sf::read_sf(layer = 'basin')
-  return(st_rename_geometry(cn_weihe_basin,'geometry'))
+  suppressWarnings({
+    cn_weihe_basin = system.file('extdata/weihe.gdb',package = 'geocn') |>
+    sf::read_sf(layer = 'basin') |>
+    st_rename_geometry('geometry') |>
+    sf::st_cast('POLYGON')
+    })
+  return(cn_weihe_basin)
 }
 
 #' Loading the Boundary Polygon Data of Loess Plateau
